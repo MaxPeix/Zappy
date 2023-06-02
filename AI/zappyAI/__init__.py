2aaa8101b648343e6c05cb282eafe1ebad4f060c
RedCommand <maxime.senard@outlook.com>
2023-06-02 13:08:59 +0200

    [ADD] objects utils in imports

--- AI/zappyAI/__init__.py
+++ AI/zappyAI/__init__.py
@@ -1,4 +1,4 @@
-from zappyAI.objects import ObjectType, Object
+from zappyAI.objects import ObjectType, Object, get_object
 from zappyAI.enums import *
 from zappyAI.tiles import Tile
 from zappyAI.struct import Pos, Size
