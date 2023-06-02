9bc29d78973a4a7f6122851a3c0a0c9367396ee2
RedCommand <maxime.senard@outlook.com>
2023-06-02 15:56:26 +0200

    [FIX] better attribute to tiles

--- AI/zappyAI/tiles.py
+++ AI/zappyAI/tiles.py
@@ -1,9 +1,17 @@
-from zappyAI.objects import Object
+from zappyAI.objects import Object, Player
 from dataclasses import dataclass
 
 
 @dataclass
 class Tile:
-    x: int
-    y: int
+    def __str__(self):
+        if not self.known:
+            return "?"
+        if len(self.objects) == 0:
+            return " "
+        elif Player in self.objects:
+            return "P"
+        else:
+            return "!"
+    known: bool
     objects: list[Object]
