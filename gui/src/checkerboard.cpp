7b6bc6d6d5c822b7a3b24a425b45c08ebef074ad
Hugo Dubois <duboish00@gmail.com>
2023-06-05 13:19:23 +0300

    [ADD] new buttons and text

--- gui/src/checkerboard.cpp
+++ gui/src/checkerboard.cpp
@@ -9,40 +9,101 @@
 
 void draw_checkerboard(int x, int y)
 {
-    int taille_case = 1080 / (y + 2);
-    int largeur_damier = taille_case * x;
-    int largeur_rectangle = 1920 - largeur_damier;
-
+    int box_size = 1080 / y;
+    int width_checkerboard = box_size * x;
+    int rectangle_width = 1920 - width_checkerboard;
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Trantor");
-    sf::RectangleShape rectangle(sf::Vector2f(taille_case, taille_case));
+    sf::RectangleShape rectangle(sf::Vector2f(box_size, box_size));
+    sf::Font font;
 
-    sf::RectangleShape topRectangle(sf::Vector2f(1920, taille_case * 1.5));
-    topRectangle.setPosition(0, 0);
-    topRectangle.setFillColor(sf::Color::Green);
+    if (!font.loadFromFile("assets/Mergury-Regular.otf")) {
+        std::cerr << "Failed to load font." << std::endl;
+        return;
+    }
 
-    sf::RectangleShape leftRectangle1(sf::Vector2f(largeur_rectangle, taille_case * y / 2));
-    leftRectangle1.setPosition(0, taille_case * 1.5);
+    sf::Text text1;
+    text1.setFont(font);
+    text1.setString("Teams name/Player :");
+    text1.setCharacterSize(40);
+    text1.setFillColor(sf::Color::Black);
+    text1.setPosition(250, 10);
+
+    sf::Text text2;
+    text2.setFont(font);
+    text2.setString("Chat :");
+    text2.setCharacterSize(40);
+    text2.setFillColor(sf::Color::Black);
+    text2.setPosition(350, 1080 / 3 + 10);
+
+    sf::Text text3;
+    text3.setFont(font);
+    text3.setString("Info :");
+    text3.setCharacterSize(40);
+    text3.setFillColor(sf::Color::Black);
+    text3.setPosition(350, 2 * 1080 / 3 + 10);
+
+    sf::RectangleShape leftRectangle1(sf::Vector2f(rectangle_width, 1080 / 3));
+    leftRectangle1.setPosition(0, 0);
     leftRectangle1.setFillColor(sf::Color::Blue);
 
-    sf::RectangleShape leftRectangle2(sf::Vector2f(largeur_rectangle, taille_case * y / 2));
-    leftRectangle2.setPosition(0, taille_case * (y / 2 + 1.5));
+    sf::RectangleShape leftRectangle2(sf::Vector2f(rectangle_width, 1080 / 3));
+    leftRectangle2.setPosition(0, 1080 / 3);
     leftRectangle2.setFillColor(sf::Color::Red);
+
+    sf::RectangleShape leftRectangle3(sf::Vector2f(rectangle_width, 1080 / 3));
+    leftRectangle3.setPosition(0, 2 * 1080 / 3);
+    leftRectangle3.setFillColor(sf::Color::Green);
+
+    sf::Texture closeButtonTexture;
+    if (!closeButtonTexture.loadFromFile("assets/quit-icon.png")) {
+        std::cerr << "Failed to load close button texture." << std::endl;
+        return;
+    }
+
+    sf::Sprite closeButton;
+    closeButton.setTexture(closeButtonTexture);
+    closeButton.setPosition(10, 10);
+
+    sf::Texture optionsButtonTexture;
+    if (!optionsButtonTexture.loadFromFile("assets/options.png")) {
+        std::cerr << "Failed to load options button texture." << std::endl;
+        return;
+    }
+
+    sf::Sprite optionsButton;
+    optionsButton.setTexture(optionsButtonTexture);
+    optionsButton.setPosition(80, 10);
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    if (closeButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                        window.close();
+                    }
+                    if (optionsButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                        std::cout << "Options button clicked\n";
+                    }
+                }
+            }
         }
         window.clear();
-
-        window.draw(topRectangle);
         window.draw(leftRectangle1);
         window.draw(leftRectangle2);
+        window.draw(leftRectangle3);
+        window.draw(closeButton);
+        window.draw(optionsButton);
+        window.draw(text1);
+        window.draw(text2);
+        window.draw(text3);
 
         for(int i = 0; i < x; i++) {
             for(int j = 0; j < y; j++) {
-                rectangle.setPosition(i * taille_case + largeur_rectangle, j * taille_case + taille_case * 1.5);
+                rectangle.setPosition(i * box_size + rectangle_width, j * box_size);
                 if((i + j) % 2 == 0)
                     rectangle.setFillColor(sf::Color::Black);
                 else
