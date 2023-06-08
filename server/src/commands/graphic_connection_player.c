4414a76353a2ccb79903cf0935bd0ff6b9807b86
MaxPeix <max.peixoto@epitech.eu>
2023-06-08 16:21:24 +0200

    [FIX] Now check if the new client is the GRAPHIC before sending a new client

--- server/src/commands/graphic_connection_player.c
+++ server/src/commands/graphic_connection_player.c
@@ -26,7 +26,8 @@ void send_notification_player_loggin(client_t *clients,
     client_t *client_logged_in, server_params_t *server_params)
 {
     for (int i = 0; i < MAX_CLIENTS; i++)
-        if (clients[i].is_graphical == 1)
+        if (clients[i].is_graphical == 1 &&
+            client_logged_in->is_graphical == 0)
             send_notif_to_graphic_on_loggin(client_logged_in,
                 clients[i].socket);
 }
