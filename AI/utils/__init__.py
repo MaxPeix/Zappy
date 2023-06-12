cb12705408b0588916db316161425c962fa9fc8a
RedCommand <maxime.senard@outlook.com>
2023-06-12 10:39:48 +0200

    [ADD] encryption utils

--- AI/utils/__init__.py
+++ AI/utils/__init__.py
@@ -1,1 +1,2 @@
 from utils.connection import Comm
+from utils.crypt import encrypt, decrypt, generate_key
---
+++ AI/utils/crypt.py
@@ -0,0 +1,17 @@
+from cryptography.fernet import Fernet
+import hashlib
+import base64
+
+
+def generate_key(password: str) -> bytes:
+    return base64.b64encode(hashlib.sha512(password.encode("utf-8")).hexdigest()[0:32].encode("utf-8"))
+
+
+def encrypt(message: bytes, key: bytes) -> bytes:
+    print(key)
+    print(len(key))
+    return Fernet(key).encrypt(message)
+
+
+def decrypt(token: bytes, key: bytes) -> bytes:
+    return Fernet(key).decrypt(token)
