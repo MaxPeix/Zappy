a948d58bf90dae4e18b13878000b38f531ec6e94
melissa-laget <melissa.laget@epitech.eu>
2023-06-12 12:17:41 +0200

    [FIX] removing 'ko' print

--- server/src/commands/handle_command.c
+++ server/src/commands/handle_command.c
@@ -6,88 +6,79 @@
 */
 
 #include "server.h"
-#include <stdbool.h>
 
-bool handle_command_five(client_t *client,
+void handle_command_five(client_t *client,
                          server_params_t *server_params,
                          char **args)
 {
     if (strcasecmp(args[0], "TAKE") == 0) {
         take_command(server_params, client, args);
-        return true;
+        return;
     }
     if (strcasecmp(args[0], "SET") == 0) {
         set_command(server_params, client, args);
-        return true;
+        return;
     }
-    return false;
 }
 
-bool handle_command_four(client_t *client,
+void handle_command_four(client_t *client,
                          server_params_t *server_params,
                          char **args)
 {
     if (strcasecmp(args[0], "BCT") == 0) {
         print_bct(server_params, client, args);
-        return true;
+        return;
     }
     if (strcasecmp(args[0], "FORWARD") == 0) {
         handle_forward_command(client, args);
-        return true;
+        return;
     }
     if (strcasecmp(args[0], "RIGHT") == 0) {
         handle_right_command(client, args);
-        return true;
     }
     if (strcasecmp(args[0], "LEFT") == 0) {
         handle_left_command(client, args);
-        return true;
+        return;
     }
-    if (handle_command_five(client, server_params, args) == true)
-        return true;
-    return false;
+    handle_command_five(client, server_params, args);
 }
 
-bool handle_command_three(client_t *client,
+void handle_command_three(client_t *client,
                           server_params_t *server_params,
                           char **args)
 {
     if (strcasecmp(args[0], "SGT") == 0) {
         print_sgt(server_params, client);
-        return true;
+        return;
     }
     if (strcasecmp(args[0], "MCT") == 0) {
         print_mct(server_params, client);
-        return true;
+        return;
     }
     if (strcasecmp(args[0], "MSZ") == 0) {
         print_msz(server_params, client);
-        return true;
+        return;
     }
     if (strcasecmp(args[0], "TNA") == 0) {
         print_tna(server_params, client);
-        return true;
+        return;
     }
-    if (handle_command_four(client, server_params, args) == true)
-        return true;
-    return false;
+    handle_command_four(client, server_params, args);
 }
 
-bool handle_command_two(client_t *client,
+void handle_command_two(client_t *client,
                         server_params_t *server_params,
                         char **args)
 {
     if (strcasecmp(args[0], "INVENTORY") == 0) {
         print_inventory(server_params, client);
-        return true;
+        return;
     }
     if (strcasecmp(args[0], "GRAPHIC") == 0) {
         handle_graphic_command(client, server_params);
-        return true;
+        return;
     }
-    if (handle_command_three(client, server_params, args) == true)
-        return true;
-    return false;
+    handle_command_three(client, server_params, args);
 }
 
 void handle_command(client_t *client,
@@ -109,8 +100,5 @@ void handle_command(client_t *client,
         handle_disconnect(client);
         return;
     }
-    if (handle_command_two(client, server_params, args) == true)
-        return;
-    else
-        send_response(client_socket, "ko\n");
+    handle_command_two(client, server_params, args);
 }
