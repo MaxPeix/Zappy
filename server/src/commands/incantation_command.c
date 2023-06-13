404eb5a8e05f49d1232198b36b1bbd7ccbadfa68
melissa-laget <melissa.laget@epitech.eu>
2023-06-13 15:53:37 +0200

    [FIX] stone gets removed if incantation was done
    View #2

--- server/src/commands/incantation_command.c
+++ server/src/commands/incantation_command.c
@@ -23,13 +23,24 @@ static bool check_stone(tile_t *tile, int level)
         || tile->mendiane < mendiane[level - 1]
         || tile->thystame < thystame[level - 1])
         return false;
-    tile->linemate -= linemate[level - 1];
-    tile->deraumere -= deraumere[level - 1];
-    tile->sibur -= sibur[level - 1];
-    tile->mendiane -= mendiane[level - 1];
-    tile->phiras -= phiras[level - 1];
-    tile->thystame -= thystame[level - 1];
     return true;
+}
+
+static void remove_stones(tile_t *tile, int level)
+{
+    int linemate[7] = { 1, 1, 2, 1, 1, 1, 1 };
+    int deraumere[7] = { 0, 1, 0, 1, 2, 2, 2 };
+    int sibur[7] = { 0, 1, 1, 2, 1, 3, 2 };
+    int mendiane[7] = { 0, 0, 0, 0, 3, 0, 2 };
+    int phiras[7] = { 0, 0, 2, 1, 0, 1, 2 };
+    int thystame[7] = { 0, 0, 0, 0, 0, 0, 1 };
+
+    tile->linemate -= linemate[level - 2];
+    tile->deraumere -= deraumere[level - 2];
+    tile->sibur -= sibur[level - 2];
+    tile->mendiane -= mendiane[level - 2];
+    tile->phiras -= phiras[level - 2];
+    tile->thystame -= thystame[level - 2];
 }
 
 void handle_incantation_command(client_t *clients,
@@ -45,8 +56,10 @@ void handle_incantation_command(client_t *clients,
     if (strcasecmp(args[0], "INCANTATION") != 0)
         return;
     for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].is_connected == 0 || clients[i].is_graphical == 1)
+            continue;
         if (clients[i].x_position == client->x_position
-            && clients[i].y_position == client->x_position
+            && clients[i].y_position == client->y_position
             && strcmp(clients[i].team_name, client->team_name) == 0)
             nb_player_on_tile++;
     }
@@ -55,6 +68,7 @@ void handle_incantation_command(client_t *clients,
             && check_stone(tile, client->level) == true) {
             client->level++;
             dprintf(client->socket, "Current level: %d\n", client->level);
+            remove_stones(tile, client->level);
         }
     }
 }
@@ -68,8 +82,10 @@ int can_do_incantation(client_t *clients,
     int nb_player_on_tile = 0;
 
     for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].is_connected == 0 || clients[i].is_graphical == 1)
+            continue;
         if (clients[i].x_position == client->x_position
-            && clients[i].y_position == client->x_position
+            && clients[i].y_position == client->y_position
             && strcmp(clients[i].team_name, client->team_name) == 0)
             nb_player_on_tile++;
     }
@@ -99,8 +115,10 @@ void send_message_to_graphical_start_incantation(client_t *clients,
 
     int nb_player_on_tile = 0;
     for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].is_connected == 0 || clients[i].is_graphical == 1)
+            continue;
         if (clients[i].x_position == client->x_position
-            && clients[i].y_position == client->x_position
+            && clients[i].y_position == client->y_position
             && strcmp(clients[i].team_name, client->team_name) == 0) {
             char player_num[10];
             sprintf(player_num, " %d", clients[i].id);
