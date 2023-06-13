41b873c8f7ffadea7ea9ff2eead2a3720d4afdbf
RedCommand <maxime.senard@outlook.com>
2023-06-13 22:50:23 +0200

    [ADD] new test main

--- AI/main.py
+++ AI/main.py
@@ -1,13 +1,4 @@
-import zappyAI as zp
-
 from brain import Brain
-import json
-
 br = Brain("team1", '127.0.0.1', 8000)
 
-lala = br._ai.to_json()
-
-print(lala)
-
-# br.cartography(zp.Pos(1, 6), zp.Pos(6, 1))
-
+br.run()
