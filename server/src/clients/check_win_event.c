8e501a32c97657bc1344dcc4f0c6a5137be2ccfb
melissa-laget <melissa.laget@epitech.eu>
2023-06-20 20:44:22 +0200

    [FIX] is_graphical check

--- server/src/clients/check_win_event.c
+++ server/src/clients/check_win_event.c
@@ -7,11 +7,20 @@
 
 #include "server.h"
 
+void count_nb_player(client_t *client, server_params_t *server_params,
+    int team_count, int *team)
+{
+    for (int j = 0; j < team_count; j++)
+        if (strcmp(client->team_name, server_params->team_names[j]) == 0)
+            team[j] += 1;
+}
+
 void check_win_event(client_t client, client_t *clients,
-                     server_params_t *server_params)
+    server_params_t *server_params)
 {
     int team_count = 0;
     int *team = NULL;
+    char *output_to_graphical = NULL;
 
     for (;server_params->team_names[team_count] != NULL; team_count++);
     team = malloc(sizeof(int) * team_count);
@@ -20,14 +29,13 @@ void check_win_event(client_t client, client_t *clients,
             || clients[i].is_dead == 1)
             continue;
         if (clients[i].level == 8) {
-            for (int j = 0; j < team_count; j++)
-                if (strcmp(clients[i].team_name, server_params->team_names[j]) == 0)
-                    team[j] += 1;
+            count_nb_player(&clients[i], server_params, team_count, team);
         }
     }
     for (int i = 0; i < team_count; i++)
         if (team[i] >= 6) {
-            send_message_to_graphical(clients, "seg\n");
+            output_to_graphical = msprintf("seg %s\n", server_params->team_names[i]);
+            send_message_to_graphical(clients, output_to_graphical);
             return;
         }
 }
